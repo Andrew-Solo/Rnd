b7eaf0ecea9cefb81b80cf94485b706a6de3c578
AndrewSolo <asolomonov@optimajet.com>
2022-07-17 02:08:34 +0300

    Created all common controller actions

--- RnDBot/Controllers/MainController.cs
+++ RnDBot/Controllers/MainController.cs
@@ -1,6 +1,14 @@
-﻿namespace RnDBot.Controllers;
+﻿using Discord.Interactions;
 
-public class MainController
+namespace RnDBot.Controllers;
+
+public class MainController : InteractionModuleBase<SocketInteractionContext>
 {
-    
+    public Task CharacterAttributesAsync() { return Task.CompletedTask; }
+    public Task CharacterPointsAsync() { return Task.CompletedTask; }
+    public Task CharacterSkillsAsync() { return Task.CompletedTask; }
+    public Task RollSkillAsync() { return Task.CompletedTask; }
+    public Task RollDiceAsync() { return Task.CompletedTask; }
+    public Task PointAlterAsync() { return Task.CompletedTask; }
+    public Task PointRestAsync() { return Task.CompletedTask; }
 }
---
+++ RnDBot/Controllers/PointController.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Interactions;
+
+namespace RnDBot.Controllers;
+
+[Group("point", "Команды для управления состояниями и очками персонажа")]
+public class PointController : InteractionModuleBase<SocketInteractionContext>
+{
+    [AutocompleteCommand("name", "alter")]
+    public Task PointAlterAutocomplete() { return Task.CompletedTask; }
+    
+    [SlashCommand("alter", "Изменение выбранного состояния или очков на указанное значение")]
+    public Task AlterAsync(
+        [Summary("Состояние", "Название состояния/очков для изменения")][Autocomplete] string name,
+        [Summary("Значение", "Значение на которое изменится состояние")] int number) 
+    { return Task.CompletedTask; }
+    
+    [AutocompleteCommand("name", "refresh")]
+    public Task PointRefreshAutocomplete() { return Task.CompletedTask; }
+    
+    [SlashCommand("refresh", "Устанавливает знаечние по умолчанию всех очков и состояний")]
+    public Task RefreshAsync([Summary("Состояние", "Название состояния/очков для изменения")][Autocomplete] string name) 
+    { return Task.CompletedTask; }
+    
+    [SlashCommand("rest", "Активирует длительный отдых, очки способностей тратяться на худшее состояние")]
+    public Task RestAsync() { return Task.CompletedTask; }
+}
