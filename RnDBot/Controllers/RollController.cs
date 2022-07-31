a5b9f818705b38b0623be6a3d3fa966706323d7d
AndrewSolo <asolomonov@optimajet.com>
2022-07-31 16:53:12 +0200

    roll dice implementation

--- RnDBot/Controllers/RollController.cs
+++ RnDBot/Controllers/RollController.cs
@@ -1,4 +1,10 @@
-﻿using Discord.Interactions;
+﻿using System.Globalization;
+using Dice;
+using Discord.Interactions;
+using RnDBot.Controllers.Helpers;
+using RnDBot.Models.Common;
+using RnDBot.Models.Glossaries;
+using RnDBot.Views;
 
 namespace RnDBot.Controllers;
 
@@ -6,22 +12,42 @@ namespace RnDBot.Controllers;
 public class RollController : InteractionModuleBase<SocketInteractionContext>
 {
     [SlashCommand("dice", "Выполняет бросок дайсов согласно нотации бросков")]
-    public Task DiceAsync([Summary("Нотация", "Нотация броска, по которой будет сгенерирован результат")] string notation) 
-    { return Task.CompletedTask; }
+    public async Task DiceAsync([Summary("Нотация", "Нотация броска, по которой будет сгенерирован результат")] string notation)
+    {
+        var result = Roller.Roll(notation);
 
-    [AutocompleteCommand("name", "skill")]
-    public Task SkillNameAutocomplete() { return Task.CompletedTask; }
-    
-    [AutocompleteCommand("attribute", "skill")]
-    public Task SkillAttributeAutocomplete() { return Task.CompletedTask; }
+        var panel = new CommonPanel($"Бросок {notation}", 
+            new TextField<decimal>("Результат", result.Value),
+            new ListField("Кости", result.Values.Select(d => d.Value.ToString(CultureInfo.InvariantCulture))));
+
+        await RespondAsync(embed: EmbedView.Build(panel));
+    }
+
+    [AutocompleteCommand("навык", "skill")]
+    public async Task SkillNameAutocomplete()
+    {
+        var autocomplete = new Autocomplete<string>(Context, 
+            Glossary.AncorniaSkillNamesReversed.Keys, 
+            s => s);
+        
+        await autocomplete.RespondAsync();
+    }
+
+    [AutocompleteCommand("атрибут", "skill")]
+    public async Task AttributeNameAutocomplete()
+    {
+        var autocomplete = new Autocomplete<string>(Context, 
+            Glossary.AttributeNamesReversed.Keys, 
+            s => s);
+        
+        await autocomplete.RespondAsync();
+    }
     
     [SlashCommand("skill", "Выполняет проверку указанного навыка, используя модификатор выбранного аттрибута")]
     public Task SkillAsync(
-        [Summary("Навык", "Название проверяемого навыка")][Autocomplete] string name, 
-        [Summary("Атрибут", "Название атрибута для модификатора проврки")][Autocomplete] string? attribute = null, 
-        [Summary("Преимущества", "Количество преимуществ или помех при отрицательном значении")] int? advantages = 0, 
-        [Summary("Состояние", "Дополнительный модификатор броска")] int? modifier = 0) 
+        [Summary("навык", "Название проверяемого навыка")][Autocomplete] string skillName, 
+        [Summary("атрибут", "Название атрибута для модификатора проверки")][Autocomplete] string? attributeName = null, 
+        [Summary("преимущества", "Количество преимуществ или помех при отрицательном значении")] int? advantages = 0, 
+        [Summary("модификатор", "Дополнительный модификатор броска")] int? modifier = 0) 
     { return Task.CompletedTask; }
-    
-    // public Task AbilityAsync() { return Task.CompletedTask; }
 }
