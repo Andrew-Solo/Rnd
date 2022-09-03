364aed3305ec94bbce7af74878b5e11d25ec1921
AndrewSolo <asolomonov@optimajet.com>
2022-09-03 20:57:40 +0200

    User controller

---
+++ RnDApi/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RnDApi.Data.Entities;
+
+namespace RnDApi.Data;
+
+public sealed class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+    
+    public DbSet<User> Users { get; set; } = null!;
+}
---
+++ RnDApi/Data/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace RnDApi.Data.Entities;
+
+[Index(nameof(Login), IsUnique = true)]
+public class User
+{
+    public User(Guid id, string login, string passwordHash)
+    {
+        Id = id;
+        Login = login;
+        PasswordHash = passwordHash;
+    }
+    
+    public User(string login, string passwordHash)
+    {
+        Id = Guid.NewGuid();
+        Login = login;
+        PasswordHash = passwordHash;
+    }
+
+    public Guid Id { get; set; } 
+    
+    [Required]
+    public string Login { get; set; }
+    
+    [Required]
+    public string PasswordHash { get; set; }
+}
