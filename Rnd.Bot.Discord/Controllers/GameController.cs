be433dbc5c9c039631548bb069a6361c967c9a41
AndrewSolo <91146748+Andrew-Solo@users.noreply.github.com>
2022-12-03 22:02:02 +0100

    Game controller implementation

--- Rnd.Bot.Discord/Controllers/GameController.cs
+++ Rnd.Bot.Discord/Controllers/GameController.cs
@@ -8,14 +8,13 @@ using Rnd.Bot.Discord.Views.Panels;
 
 namespace Rnd.Bot.Discord.Controllers;
 
-[Group("user", "Команды для управления играми")]
+[Group("game", "Команды для управления играми")]
 public class GameController : InteractionModuleBase<SocketInteractionContext>
 {
     //Dependency Injections
-    public DataContext Data { get; set; } = null!;
     public SessionProvider Provider { get; set; } = null!;
     
-    [AutocompleteCommand("gameId", "show")]
+    [AutocompleteCommand("game-id", "show")]
     public async Task GameNameAutocomplete()
     {
         var client = await Provider.GetClientAsync(Context.User.Id);
@@ -24,19 +23,19 @@ public class GameController : InteractionModuleBase<SocketInteractionContext>
 
         var games = await client.Games.ListOrExceptionAsync();
         
-        var autocomplete = new Autocomplete<GameModel>(games, g => g.Name, g => g.Id);
+        var autocomplete = new Autocomplete<GameModel>(games, g => g.Name, g => g.Id.ToString());
         
         await autocomplete.RespondAsync(Context);
     }
     
     [SlashCommand("show", "Показать данные игры")]
-    public async Task ShowAsync(Guid gameId)
+    public async Task ShowAsync(string gameId)
     {
         var client = await Provider.GetClientAsync(Context.User.Id);
         
         await this.CheckAuthorized(client);
 
-        var game = await client.Games.GetOrExceptionAsync(gameId);
+        var game = await client.Games.GetOrExceptionAsync(new Guid(gameId));
 
         await this.EmbedResponseAsync(PanelBuilder.WithTitle("Игра").ByClass(game));
     }
@@ -51,5 +50,61 @@ public class GameController : InteractionModuleBase<SocketInteractionContext>
         var games = await client.Games.ListOrExceptionAsync();
 
         await this.EmbedResponseAsync(FieldBuilder.WithName("Мои игры").WithValue(games.Select(g => g.Name)).Build().AsPanel());
+    }
+
+    [SlashCommand("create", "Создать игру")]
+    public async Task CreateAsync(string name, string? title = null, string? description = null)
+    {
+        var client = await Provider.GetClientAsync(Context.User.Id);
+        
+        await this.CheckAuthorized(client);
+
+        var game = new GameFormModel
+        {
+            Name = name,
+            Title = title,
+            Description = description,
+        };
+
+        var response = await client.Games.AddAsync(game);
+
+        await this.ApiResponseAsync("Игра создана", response);
+    }
+    
+    [AutocompleteCommand("game-id", "edit")]
+    public async Task GameNameEditAutocomplete() => await GameNameAutocomplete();
+    
+    [SlashCommand("edit", "Отредактировать игру")]
+    public async Task EditAsync(string gameId, string? name = null, string? title = null, string? description = null)
+    {
+        var client = await Provider.GetClientAsync(Context.User.Id);
+        
+        await this.CheckAuthorized(client);
+        
+        var game = new GameFormModel
+        {
+            Name = name,
+            Title = title,
+            Description = description,
+        };
+        
+        var response = await client.Games.EditAsync(game, new Guid(gameId));
+
+        await this.ApiResponseAsync("Игра отредактирована", response);
+    }
+ 
+    [AutocompleteCommand("game-id", "delete")]
+    public async Task GameNameDeleteAutocomplete() => await GameNameAutocomplete();
+    
+    [SlashCommand("delete", "Удалить игру")]
+    public async Task DeleteAsync(string gameId)
+    {
+        var client = await Provider.GetClientAsync(Context.User.Id);
+        
+        await this.CheckAuthorized(client);
+
+        var response = await client.Games.DeleteAsync(new Guid(gameId));
+        
+        await this.ApiResponseAsync("Игра удалено", response);
     }
 }
