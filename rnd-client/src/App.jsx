6532c8efd6f58fd1033fc96e40fdd3a9f8927df8
AndrewSolo <91146748+Andrew-Solo@users.noreply.github.com>
2023-04-16 22:52:41 +0200

    Setup router

--- rnd-client/src/App.jsx
+++ rnd-client/src/App.jsx
@@ -1,11 +1,13 @@
-﻿import {Typography} from "@mui/material";
+﻿import {Route, Routes} from "react-router-dom";
+import Root from "./pages/root/Root";
+import {Typography} from "@mui/material";
 
 export default function App () {
   return (
-    <div>
-      <Typography variant="h1">
-        Hello, World!
-      </Typography>
-    </div>
+    <Routes>
+      <Route path="/" element={<Root/>}>
+        <Route index element={<Typography variant="h2">Hi there!</Typography>}/>
+      </Route>
+    </Routes>
   );
 }
---
+++ rnd-client/src/pages/root/Root.jsx
@@ -0,0 +1,15 @@
+﻿import {Typography} from "@mui/material";
+import {Outlet} from "react-router-dom";
+
+export default function Root() {
+  return (
+    <div>
+      <Typography variant="h1">
+        Hello, World!
+      </Typography>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
