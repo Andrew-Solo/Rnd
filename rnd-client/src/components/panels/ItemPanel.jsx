7d780f1050705a571ff90762dbd33113c2953e9e
AndrewSolo <91146748+Andrew-Solo@users.noreply.github.com>
2023-06-07 19:46:22 +0200

    GamePAge improvements

--- rnd-client/src/components/panels/ItemPanel.jsx
+++ rnd-client/src/components/panels/ItemPanel.jsx
@@ -1,12 +1,22 @@
-﻿import {Box} from "@mui/material";
+﻿import {Box, Stack} from "@mui/material";
 import ItemHeader from "./ItemHeader";
 
-export default function ItemPanel({children, ...props}) {
+export default function ItemPanel({children, data, actions, ...props}) {
   return (
     <Box component="main" width={1} display="flex" flexDirection="column">
       <ItemHeader {...props}/>
       <Box padding={4}>
-        {children}
+        <Stack maxWidth={750} gap={4}>
+          <Box display="flex" gap={4}>
+            <Box width={7.5/10} minWidth={150}>
+              {children[0]}
+            </Box>
+            <Box width={2.5/10} minWidth={100}>
+              {children[1]}
+            </Box>
+          </Box>
+          {children[2]}
+        </Stack>
       </Box>
     </Box>
   );
--- rnd-client/src/pages/games/GamePage.jsx
+++ rnd-client/src/pages/games/GamePage.jsx
@@ -1,11 +1,20 @@
 ﻿import ItemPanel from "../../components/panels/ItemPanel";
+import {Box, Paper} from "@mui/material";
 
 export default function GamePage () {
   const game = {name: "mrak", title: "Мрак", owner: "AndrewSolo", image: "https://cdn.discordapp.com/attachments/1104404469090881556/1113971170581155900/c0a31dc92f19f998.png"};
 
   return (
     <ItemPanel {...game} subtitle={game.owner}>
-      New component
+      <Paper elevation={3}>
+        <Box height={300}/>
+      </Paper>
+      <Paper elevation={3}>
+        <Box height={300}/>
+      </Paper>
+      <Paper elevation={3}>
+        <Box height={300}/>
+      </Paper>
     </ItemPanel>
   );
 }
